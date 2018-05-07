aef7fa07ce14633911605bb93d897fa849be83dc
haruka9147 <haruphonexxx@gmail.com>
2018-05-07 13:21:22 -0700

    commit lesson2

---
+++ Lesson2/Lesson2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson2
+//
+//  Created by 北田晴佳 on 2018/05/07.
+//  Copyright © 2018 com.Haruka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Sw1/Sw1/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Sw1
+//
+//  Created by 北田晴佳 on 2018/05/03.
+//  Copyright © 2018 com.Haruka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func fillterStrings(A numbers: [Int], B includeNumber: (Int) -> Bool) -> [String] {
+    return ["Hello"]
+}
+
+// fillterStrings(A:, B:)
+
+
+
